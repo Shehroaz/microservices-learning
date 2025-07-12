67969a21a3f7b014f34932ed6a692d50225f7351
Shehroaz Aslam <muhammadshehroaz@gmail.com>
2025-07-12 14:58:29 +0500

    removed some annotations

--- accounts-service/src/main/java/com/sherry/accounts_service/entity/BaseEntity.java
+++ accounts-service/src/main/java/com/sherry/accounts_service/entity/BaseEntity.java
@@ -15,23 +15,18 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 @Getter @Setter @ToString
 public class BaseEntity {
 
-    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
-    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
 
-    @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime updatedAt;
 
-    @LastModifiedBy
     @Column(insertable = false)
     private String updatedBy;
 }
